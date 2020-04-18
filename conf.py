48e76d10065ac12f6caa56213fb2cbc9848288b6
kongsny <ksnyweb@163.com>
2020-04-18 23:07:22 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@
 """
 
 # For Maverick
-site_prefix = "/"
+site_prefix = "/Second_Blog/"
 source_dir = "../src/"
 build_dir = "../dist/"
 index_page_size = 10
@@ -21,11 +21,11 @@ enable_jsdelivr = {
 # 站点设置
 site_name = "空山鸟语"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
+site_build_date = "2020-04-18T16:51+08:00"
 author = "空山鸟语"
 email = "hi@imalan.cn"
 author_homepage = "https://www.imalan.cn"
-description = "杨帆一行"
+description = "扬帆起航"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
